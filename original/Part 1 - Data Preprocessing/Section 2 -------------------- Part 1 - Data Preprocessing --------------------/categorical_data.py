0746db5fde6d5fd6a9a555bb603b62eed0560fb2
EmanuelMaya09 <59615812+EmanuelMaya09@users.noreply.github.com>
2024-01-23 17:36:42 -0500

    update files py

--- original/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/categorical_data.py
+++ original/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/categorical_data.py
@@ -13,25 +13,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importar el data set
+#importar el data set
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
-# Codificar datos categóricos
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+# Codificar datos categoricos X
+from sklearn import preprocessing
+LabelEncoder_X = preprocessing.LabelEncoder()
+X[:, 0] = LabelEncoder_X.fit_transform(X[:, 0])
+
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
-
-labelencoder_X = LabelEncoder()
-X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
-
+ 
 ct = ColumnTransformer(
-    [('one_hot_encoder', OneHotEncoder(categories='auto'), [0])],    # The column numbers to be transformed (here is [0] but can be [0, 1, 3])
-    remainder='passthrough'                         # Leave the rest of the columns untouched
+    [('one_hot_encoder', OneHotEncoder(categories='auto'), [0])],   
+    remainder='passthrough'                        
 )
+X = np.array(ct.fit_transform(X), dtype=np.float64)
 
-#onehotencoder = OneHotEncoder(categorical_features=[0])
-#X = onehotencoder.fit_transform(X).toarray()
-X = np.array(ct.fit_transform(X), dtype=np.float)
-labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)
+# Codificar datos categoricos y
+LabelEncoder_y = preprocessing.LabelEncoder()
+y = LabelEncoder_y.fit_transform(y)
--- original/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ original/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -13,19 +13,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importar el data set
+#importar el data set
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
-
-# Dividir el data set en conjunto de entrenamiento y conjunto de testing
+#dividir el data set en conjunto de entrenamiento y conjunto de testing
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_test, y_test = train_test_split(X, y, test_size = 0.2, random_state = 7)
 
-
-# Escalado de variables
-from sklearn.preprocessing import StandardScaler
+# Escalado de variabls
+"""from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
+X_test = sc_X.transform(X_test)"""
+
+
+
+
+
+
+
